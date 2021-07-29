a5bbcc70165c20d9a14f88c9ce2b6507f773476e
voronzz <87975300+voronzz@users.noreply.github.com>
2021-07-30 00:26:45 +0300

    Add files via upload

--- project_abito/js/main.js
+++ project_abito/js/main.js
@@ -11,3 +11,13 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("cliсk", () => {
+    menu.classList.toggle("is-open");
+    // alert("вы кликнули по кнопке меню!");
+});
+menuCloseBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+});
